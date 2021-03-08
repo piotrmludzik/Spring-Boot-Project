9ffc84a451ab27943a20edf97154240c9ad6d0ed
Piotr Młudzik <piotr.mludzik@gmail.com>
2021-03-08 10:35:57 +0100

    init the project

---
+++ src/main/java/com/codecool/springbootproject/SpringbootprojectApplication.java
@@ -0,0 +1,15 @@
+package com.codecool.springbootproject;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class SpringbootprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootprojectApplication.class, args);
+	}
+
+}
