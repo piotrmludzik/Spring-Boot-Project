524c78e75f92a972e7dd6d0e88c780c6a3ad0869
Magdalena Podolewska <magdalena@podolewska.com>
2021-03-10 15:15:11 +0100

    feat: added some logger info

--- src/main/java/com/codecool/springbootproject/config/SpringBootProjectApp.java
+++ src/main/java/com/codecool/springbootproject/config/SpringBootProjectApp.java
@@ -1,12 +1,15 @@
 package com.codecool.springbootproject.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringBootProjectApp {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApp.class, args);
+		log.info("start the app");
 	}
 
 }
